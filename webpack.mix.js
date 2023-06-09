f6d8491807628022eb1d9ec06628a72be791de26
Jp-dsouls <werras04@gmail.com>
2023-06-09 13:09:01 -0500

    fix: mix add js/css

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,13 +12,22 @@ mix.sourceMaps();
  |
  */
 
-
 mix.js('resources/js/app.js', 'public/js')
     .js('node_modules/popper.js/dist/popper.js', 'public/js').sourceMaps()
     .js('node_modules/jquery/dist/jquery.min.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .css('resources/css/app.css', 'public/css')
-    .vue();
+    .js('node_modules/bootstrap-vue/dist/bootstrap-vue.js', 'public/js')
+    .js('node_modules/sweetalert2/src/sweetalert2.js', 'public/js')
 
-    
+    .sass('resources/sass/app.scss', 'public/css')
+    // .sass('resources/sass/sweetalert2.scss', 'public/css')
+    // .sass('resources/sass/bootstrap.scss', 'public/css')
+
+    .css('resources/css/app.css', 'public/css')
+    // .css('resources/css/bootstrap-vue.css', 'public/css')
+
+
+    .copy('node_modules/@fortawesome/fontawesome-free/css/all.min.css', 'public/css/fontawesome.css')
+    .copyDirectory('node_modules/@fortawesome/fontawesome-free/webfonts', 'public/webfonts')
+
+    .vue();
 mix.disableSuccessNotifications();
